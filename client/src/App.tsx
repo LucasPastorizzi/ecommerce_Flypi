44a5ad0c748aa42a51696b9ca73aaee1a2727502
Lucas Pastorizzi <Lucasdasilvapastorizzi@gmail.com>
2025-10-21 22:07:23 -0300

    Fix error 404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
+import { Router as WouterRouter, Route, Switch } from "wouter";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import DashboardLayout from "./components/DashboardLayout";
@@ -16,21 +16,20 @@ import EstatisticasPage from "./pages/EstatisticasPage";
 import UsuariosPage from "./pages/UsuariosPage";
 import ConfiguracoesPage from "./pages/ConfiguracoesPage";
 
-function Router() {
+function Routes() {
   return (
     <Switch>
-      <Route path={"/"} component={VendorPage} />
-      <Route path={"/pedidos"} component={PedidosPage} />
-      <Route path={"/produtos"} component={ProdutosPage} />
-      <Route path={"/catalogo"} component={CatalogoPage} />
-      <Route path={"/clientes"} component={ClientesPage} />
-      <Route path={"/historico"} component={HistoricoPage} />
-      <Route path={"/financas"} component={FinancasPage} />
-      <Route path={"/estatisticas"} component={EstatisticasPage} />
-      <Route path={"/usuarios"} component={UsuariosPage} />
-      <Route path={"/configuracoes"} component={ConfiguracoesPage} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
+      <Route path="/" component={VendorPage} />
+      <Route path="/pedidos" component={PedidosPage} />
+      <Route path="/produtos" component={ProdutosPage} />
+      <Route path="/catalogo" component={CatalogoPage} />
+      <Route path="/clientes" component={ClientesPage} />
+      <Route path="/historico" component={HistoricoPage} />
+      <Route path="/financas" component={FinancasPage} />
+      <Route path="/estatisticas" component={EstatisticasPage} />
+      <Route path="/usuarios" component={UsuariosPage} />
+      <Route path="/configuracoes" component={ConfiguracoesPage} />
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -42,9 +41,12 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <DashboardLayout>
-            <Router />
-          </DashboardLayout>
+          {/* 👇 Adicione o base aqui */}
+          <WouterRouter base="/ecommerce_Flypi">
+            <DashboardLayout>
+              <Routes />
+            </DashboardLayout>
+          </WouterRouter>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
@@ -52,4 +54,3 @@ function App() {
 }
 
 export default App;
-
